637f3a5c435d35a2d5f90d77ea243d69aff1b68c
zahrafayya <zahra.fayyadiyati@gmail.com>
2022-09-16 08:15:30 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <Counter/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+/* background: rgb(238,130,238);
+background: linear-gradient(251deg, rgba(238,130,238,1) 0%, rgba(166,45,166,1) 30%, rgba(128,0,128,1) 71%, rgba(102,45,145,1) 100%);  */
+
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        {{displayHours}}
+        {{displayMinutes}}
+        {{displaySeconds}}
+    </div>
+</template>
+
+<script>
+export default {
+    data: ()=>({
+        displayHours: 0,
+        displayMinutes: 0,
+        displaySeconds: 0
+    }),
+    methods: {
+        showTime() {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
